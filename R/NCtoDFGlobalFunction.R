3fdbe6cd886335a14f52684f636f7ce65a3b6916
JohanFR198 <johan.tec.cslg@gmail.com>
2019-07-07 17:02:39 -0600

    Added  imports

--- R/NCtoDFGlobalFunction.R
+++ R/NCtoDFGlobalFunction.R
@@ -7,6 +7,7 @@
 #' @param PATH Is the directory where is located the .nc files of a same variable
 #' @param TYPE Is the origin of the data, set TYPE="R" if origin  is the regional  model or TYPE="G" if the  origin is  the global model
 #' @examples
+#' @import dplyr
 #' NC2DFG("~/PrecipitationGlobalModelData",TYPE="G")
 #'
 
