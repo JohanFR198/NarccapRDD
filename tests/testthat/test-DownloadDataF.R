33b9e91e51d65c71b18276d7ccc75dec07d7aa77
JohanFR198 <johan.tec.cslg@gmail.com>
2019-08-21 13:33:39 -0600

    Tests added

---
+++ tests/testthat/test-DownloadDataF.R
@@ -0,0 +1,5 @@
+context("test-DownloadDataF")
+
+test_that("Function downloads the data", {
+  expect_equal(2 * 2, 4)
+})
--- tests/testthat/test-joinRGdata.R
+++ tests/testthat/test-joinRGdata.R
@@ -1,3 +1,6 @@
 test_that("Function joins data frames", {
-  expect_equal(2 * 2, 4)
+  load("data/PRECL Global.rda")
+  load("data/sic Regional.rda")
+  expect_is(joinRGdata(`PRECL Global`, `sic Regional`), "data.frame")
 })
+
